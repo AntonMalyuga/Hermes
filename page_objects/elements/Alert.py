a99a0f7a718fee0e622448cdb7da6f0e642cc1ce
AntonMalyuga <botthebest@gmail.com>
2024-04-06 23:01:55 +0300

    set testIt in classess alert and mainnavmenu

--- page_objects/elements/Alert.py
+++ page_objects/elements/Alert.py
@@ -1,3 +1,4 @@
+import testit
 from ..BasePage import BasePage
 from selenium.webdriver.common.by import By
 
@@ -7,7 +8,11 @@ class Alert(BasePage):
     _ALERT_404_STATUS = (By.CSS_SELECTOR, '#toast-container .toast-message')
 
     def get_alert_text(self) -> str:
-        return self.find_presence_element_with_wait(second=2, css_selector=self._ALERT).text
+        alert_text = str(self.find_presence_element_with_wait(second=2, css_selector=self._ALERT).text)
+        with testit.step('Get alert text {alert_text}'):
+            return alert_text
 
     def get_alert_resource_not_found(self):
-        return self.find_presence_element_with_wait(second=1, css_selector=self._ALERT_404_STATUS).text
+        alert_text = str(self.find_presence_element_with_wait(second=1, css_selector=self._ALERT_404_STATUS).text)
+        with testit.step(f'Get alert text when resource not found {alert_text}'):
+            return alert_text
--- page_objects/elements/MainNavMenu.py
+++ page_objects/elements/MainNavMenu.py
@@ -1,5 +1,6 @@
 from ..BasePage import BasePage
 from selenium.webdriver.common.by import By
+import testit
 
 
 class MainNavMenu(BasePage):
@@ -14,31 +15,43 @@ class MainNavMenu(BasePage):
     _LOCATOR_BUTTON_SEARCH_ORDER_ID = (By.CSS_SELECTOR, '#search-task-form button')
 
     def enter_order_id_in_search_order(self, order_id):
-        self.find_element(self._LOCATOR_INPUT_SEARCH_ORDER_ID).send_keys(order_id)
+        with testit.step(f'Enter order in main menu {order_id}'):
+            self.find_element(self._LOCATOR_INPUT_SEARCH_ORDER_ID).send_keys(order_id)
 
     def get_max_length_order_id_in_search_order(self):
-        return self.find_element(self._LOCATOR_INPUT_SEARCH_ORDER_ID).get_property('maxLength')
+        max_len_search = self.find_element(self._LOCATOR_INPUT_SEARCH_ORDER_ID).get_property('maxLength')
+        with testit.step(f'Get max len order id in form search order {max_len_search}'):
+            return max_len_search
 
+    @testit.step('Click search order')
     def click_search_order(self):
         self.find_element(self._LOCATOR_BUTTON_SEARCH_ORDER_ID).click()
 
-    def get_user_name(self):
-        return self.find_element(self._LOCATOR_A_FULL_NAME).text
+    def get_user_name(self) -> str:
+        user_name = str(self.find_element(self._LOCATOR_A_FULL_NAME).text)
+        with testit.step('Get user name in main menu {user_name}'):
+            return user_name
 
+    @testit.step('Click by user name in main menu')
     def click_user_name(self):
         self.find_element(self._LOCATOR_A_FULL_NAME).click()
 
+    @testit.step('Click by feedback in main menu')
     def click_feedback(self):
         self.find_presence_element_with_wait(4, self._LOCATOR_A_FEEDBACK).click()
 
+    @testit.step('Click by calendar sla in main menu')
     def click_calendar_sla(self):
         self.find_element(self._LOCATOR_A_CALENDAR_SLA).click()
 
+    @testit.step('Click by realise history in main menu')
     def click_release_history(self):
         self.find_element(self._LOCATOR_A_RELEASE_HISTORY).click()
 
+    @testit.step('Click by faq in main menu')
     def click_faq(self):
         self.find_element(self._LOCATOR_A_FAQ).click()
 
+    @testit.step('Click by notification in main menu')
     def click_notification(self):
         self.find_element(self._LOCATOR_A_NOTIFICATION).click()
