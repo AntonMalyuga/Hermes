50c23f51e65a648b36bdd2d9dcacf983b09d2336
AntonMalyuga <botthebest@gmail.com>
2024-04-16 15:26:30 +0300

    integration tests autorisation in testit

--- tests/test_autorization.py
+++ tests/test_autorization.py
@@ -1,25 +1,27 @@
 import pytest
 import testit
-
 from page_objects.elements.UserLoginForm import UserLoginForm
 
 
 @testit.title('Авторизация')
 @testit.displayName('Авторизация с валидными данными')
 @testit.description('Проверяется авторизация под рабочей учётной записи')
+@testit.workItemIds(1057)
 def test_user_authorization_success(driver, base_url):
     UserLoginForm(driver).authorization_default()
-    with testit.step('Base URL is current URL' 'Authorization success'):
+    with testit.step('Проверить корректность входа в систему' 'Вход в систему выполнен'):
         assert UserLoginForm(driver).current_url() == base_url
 
 
 @testit.title('Авторизация')
 @testit.displayName('Авторизация с невалидными данными')
-@testit.description('Проверяется отображение ошибки при некорретных учётных данных и невозможность попасть в систему под ними')
+@testit.description(
+    'Проверяется отображение ошибки при некорретных учётных данных и невозможность попасть в систему под ними')
+@testit.workItemIds(1058)
 @pytest.mark.parametrize('login, password',
                          [('MalyugaAS', 'TestPass'), ('TestUser', 'TestPass')],
                          ids=['correct_login_and_not_correct_pass', 'not_correct_login_and_not_correct_pass'])
 def test_user_authorization_failed(driver, login, password):
     UserLoginForm(driver).authorization_with(username=login, password=password)
-    with testit.step(f'Check text error "Неверные учётные данные"', 'Authorization not successful'):
+    with testit.step(f'Проверить текст ошибки "Неверные учётные данные"', 'Вход в систему не выполнен'):
         assert UserLoginForm(driver).get_text_alert_error() == 'Неверные учётные данные'
