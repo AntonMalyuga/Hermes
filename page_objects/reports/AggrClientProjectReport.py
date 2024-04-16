bc6d27b01e2a15889d32f120f38436e11f2f2ff0
AntonMalyuga <botthebest@gmail.com>
2024-04-16 15:33:49 +0300

    update tests report in testit

--- page_objects/reports/AggrClientProjectReport.py
+++ page_objects/reports/AggrClientProjectReport.py
@@ -1,4 +1,5 @@
 from ..BasePage import BasePage
+import testit
 
 
 class AggrClientProjectReport(BasePage):
@@ -7,5 +8,6 @@ class AggrClientProjectReport(BasePage):
     _CHECK_REPORT = 'button[formaction="/report/aggr_client_project_report/html"]'
 
     def check_report(self):
-        if len(self.find_elements(self._CHECK_REPORT)) > 0:
-            return True
+        with testit.step(f'Проверить открытие отчета по адресу "{self.path}", "Отчёт успешно открыт"'):
+            if len(self.find_elements(self._CHECK_REPORT)) > 0:
+                return True
--- tests/reports/test_report_aggr_client_project_report.py
+++ tests/reports/test_report_aggr_client_project_report.py
@@ -1,7 +1,11 @@
 from page_objects.reports.AggrClientProjectReport import AggrClientProjectReport
 from page_objects.elements.UserLoginForm import UserLoginForm
+import testit
 
 
+@testit.title('reports')
+@testit.displayName('Проверить открытие отчёта "Агрегированная Аналитика проекта"')
+@testit.description('Проверяется открытие отчёта "Агрегированная Аналитика проекта"')
 def test_open_report_aggr_client_project_report(driver):
     AggrClientProjectReport(driver).open()
     UserLoginForm(driver).authorization_default()
